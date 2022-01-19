fb8fe56fdf6345cdb995155b554a4a090589c7d0
rizkichairulloh <rizkim47@gmail.com>
2022-01-19 13:20:16 +0700

    change maps

--- constants/data.js
+++ constants/data.js
@@ -15,7 +15,8 @@ const userData = {
   saveDate: "Save the date of our wedding",
   descCelebration: "The Wedding Celebration of",
   maps: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.86229202924!2d110.6295842147768!3d-7.589963794526834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf07d1e67a4375a92!2zN8KwMzUnMjMuOSJTIDExMMKwMzcnNTQuNCJF!5e0!3m2!1sen!2sid!4v1642570348036!5m2!1sen!2sid",
-  mapsToApps: "https://www.google.com/maps/place/7%C2%B035'23.9%22S+110%C2%B037'54.4%22E/@-7.5899638,110.6295842,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xf07d1e67a4375a92!8m2!3d-7.5899638!4d110.6317729?hl=en",
+  mapsToApps:
+    "https://www.google.com/maps/place/7%C2%B035'23.9%22S+110%C2%B037'54.8%22E/@-7.5899787,110.6297078,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x2cc30fe00abbbab5!8m2!3d-7.5899787!4d110.6318965?hl=en",
   avatarPria: "/fuad.jpeg",
   avatarWanita: "/fia.jpeg",
 };
